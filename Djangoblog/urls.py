f19cfbfd2199cee663eae5b2d76c6b26f027f10f
alxayeed <alxayeed@gmail.com>
2019-11-20 15:41:53 +0600

    brief of django User Registraion with default User Model.Intro To Django Forms.Styling the form template with crispy form

--- Djangoblog/urls.py
+++ Djangoblog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
 from blog import urls 
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/',user_views.register,name='resgister'),
     path('', include('blog.urls')),
 
 ]
