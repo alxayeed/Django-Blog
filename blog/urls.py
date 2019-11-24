019bb3454427f79f5d0bfc92094e2e35db4e9787
alxayeed <alxayeed@gmail.com>
2019-11-24 21:16:46 +0600

    introduction to CBV with Post List and Post Details

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('',views.home,name='blog-home' ),
+    path('',views.PostListView.as_view(),name='blog-home' ),
+    path('post/<int:pk>/',views.PostDetailsView.as_view(),name='post-details' ),
     path('about/',views.about,name='blog-about' ),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
 from .models import Post 
 
-def home(request):
-	context = {
-	 'posts':Post.objects.all()
-	}
-	return render(request,'blog/home.html',context)
+# def home(request):
+# 	context = {
+# 	 'posts':Post.objects.all()
+# 	}
+# 	return render(request,'blog/home.html',context)
+
+class PostListView(ListView):
+	model = Post #The Model it will work on
+	template_name = 'blog/home.html' #overriding default template name to search
+	context_object_name = 'posts' #context with model objects
+	ordering = ['-date_posted'] #ordering newest ot oldest
+
+class PostDetailsView(DetailView):
+	model = Post 
+	
 
 def about(request):
 	return render(request,'blog/about.html',{'title':'About'})
+
+
 
 
 
