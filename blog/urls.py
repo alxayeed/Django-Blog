a223a5ed897933a5ec709825379bb906f1140cbd
alxayeed <alxayeed@gmail.com>
2019-11-24 22:01:43 +0600

    Introduction to mixin,Post Update,Post Delete

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.PostListView.as_view(),name='blog-home' ),
     path('post/<int:pk>/',views.PostDetailsView.as_view(),name='post-details' ),
+    path('post/new/',views.PostCreateView.as_view(),name='create-post' ),#app/model_form.html
+    path('post/<int:pk>/update/',views.PostUpdateView.as_view(),name='post-update' ),
+    path('post/<int:pk>/delete/',views.PostDeleteView.as_view(),name='post-delete' ),
     path('about/',views.about,name='blog-about' ),
 
 ]
