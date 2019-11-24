d427d08871733b5eefb3a7f6c852c2a5dd3da739
alxayeed <alxayeed@gmail.com>
2019-11-24 14:43:20 +0600

    list filter,list display and search options added for blog app

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,9 @@ from material.admin.decorators import register
 from .models import Post
 
 # Register your models here.
-
+@register(Post)
+class PostAdmin(MaterialModelAdmin):
+	list_display = ('title','content','author','date_posted',)
+	list_filter = ('title','content','author','date_posted',)
+	search_fields = ['title','date_posted','author__username']
 
