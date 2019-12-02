db58b011f0cc1d9a22966f0258fb30753bc0a7f8
alxayeed <alxayeed@gmail.com>
2019-12-02 19:43:06 +0600

    Clicking on a username will now show the list of that users posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.PostListView.as_view(),name='blog-home' ),
+    path('user/<str:username>/',views.UserPostListView.as_view(),name='posts-user' ),
     path('post/<int:pk>/',views.PostDetailsView.as_view(),name='post-details' ),
     path('post/new/',views.PostCreateView.as_view(),name='create-post' ),#app/model_form.html
     path('post/<int:pk>/update/',views.PostUpdateView.as_view(),name='post-update' ),
